7cc584260b6372400f69503afcf92a1f2e358fd9
RobinsonLuzo <bendanielfagan@gmail.com>
2021-08-16 15:50:16 +0100

    Profile details now available for users. Only logged in users can access certain attributes of their profile. Still no binding to html button for direct access right now. To fix: follow mechanism and that.

--- notebook/notebook/authy/views.py
+++ notebook/notebook/authy/views.py
@@ -1,32 +1,47 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
-from django.contrib.auth.models import User
-
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
-
-from authy.models import Profile
-from django.template import loader
-from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
+from django.urls import resolve
+
+from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
+from authy.models import Profile
+from post.models import Post, Follow
 
 # Create your views here.
 def UserProfile(request, username):
 	user = get_object_or_404(User, username=username)
 	profile = Profile.objects.get(user=user)
-	articles = profile.favorites.all()
+	url_name = resolve(request.path).url_name
+
+	# if profile selected then yield all posts made by user, else yield favorite/saved photos:
+	if url_name == 'profile':
+		posts = Post.objects.filter(user=user).order_by('-posted')
+	else:
+		posts = profile.favorites.all()
+
+	# Profile info:
+	posts_count = Post.objects.filter(user=user).count()
+	following_count = Follow.objects.filter(follower=user).count()
+	followers_count = Follow.objects.filter(following=user).count()
 
 	#Pagination
-	paginator = Paginator(articles, 6)
+	paginator = Paginator(posts, 6)
 	page_number = request.GET.get('page')
-	articles_paginator = paginator.get_page(page_number)
+	posts_paginator = paginator.get_page(page_number)
 
 	template = loader.get_template('profile.html')
 
 	context = {
-		'articles': articles_paginator,
-		'profile':profile,
+		'posts': posts_paginator,
+		'profile': profile,
+		'url_name': url_name,
+		'posts_count': posts_count,
+		'following_count': following_count,
+		'followers_count': followers_count,
 	}
 
 	return HttpResponse(template.render(context, request))
--- notebook/notebook/notebook/urls.py
+++ notebook/notebook/notebook/urls.py
@@ -18,8 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from authy.views import UserProfile
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
     path('user/', include('authy.urls')),
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved', UserProfile, name='profilefavorites'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
