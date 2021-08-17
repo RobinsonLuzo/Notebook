f448dd6099d4ef593a7d2d8825ce4d442f8166da
RobinsonLuzo <bendanielfagan@gmail.com>
2021-08-17 07:37:53 +0100

    Bug fix in view for follow - incorrect naming of stream on saving. Enabled follow button functionailty on frontend.

--- notebook/notebook/authy/views.py
+++ notebook/notebook/authy/views.py
@@ -2,14 +2,15 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.db import transaction
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template import loader
-from django.urls import resolve
+from django.urls import resolve, reverse
 
 from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
 from authy.models import Profile
-from post.models import Post, Follow
+from post.models import Post, Follow, Stream
 
 # Create your views here.
 def UserProfile(request, username):
@@ -28,6 +29,9 @@ def UserProfile(request, username):
 	following_count = Follow.objects.filter(follower=user).count()
 	followers_count = Follow.objects.filter(following=user).count()
 
+	# Check follow status:
+	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
 	#Pagination
 	paginator = Paginator(posts, 6)
 	page_number = request.GET.get('page')
@@ -42,6 +46,7 @@ def UserProfile(request, username):
 		'posts_count': posts_count,
 		'following_count': following_count,
 		'followers_count': followers_count,
+		'follow_status': follow_status,
 	}
 
 	return HttpResponse(template.render(context, request))
@@ -114,3 +119,36 @@ def EditProfile(request):
 	}
 
 	return render(request, 'edit_profile.html', context)
+
+
+@login_required
+def follow(request, username, option):
+	"""
+	Allow for a given user to create a following object showing them photos from another user.
+
+	If currently following that users then they should unfollow.
+	"""
+	user = request.user
+	following = get_object_or_404(User, username=username)
+
+	try:
+		f, created = Follow.objects.get_or_create(follower=user, following=following)
+
+		# if follow option flag set to 0 then unfollow:
+		if int(option) == 0:
+			f.delete()
+			Stream.objects.filter(following=following, user=user).all().delete()
+		else:
+			# return of first 10 posts is arbitrary - change if wanted.
+			posts = Post.objects.all().filter(user=following)[:10]
+
+			# See doc: https://stackoverflow.com/questions/34730385/django-rollback-save-with-transaction-atomic
+			with transaction.atomic():
+				for post in posts:
+					stream = Stream(post=post, user=user, date=post.posted, following=following)
+					stream.save()
+
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+	
+	except User.DoesNotExist:
+		return HttpResponseRedirect(reverse('profile', args=[username]))
