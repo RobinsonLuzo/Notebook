007a44cb2d3f5a6d8ad609268fb003b336e4be07
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-02 11:59:04 +0100

    Added directs url path - resolve selecting other messages in inbox.

--- notebook/notebook/direct/urls.py
+++ notebook/notebook/direct/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 # local imports
-from .views import inbox
+from .views import inbox, Directs
 
 urlpatterns = [
     path('', inbox, name='inbox'),
+    path('direct/<username>', Directs, name='directs'),
 ]
