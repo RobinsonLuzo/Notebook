14f61c444159bcdf47f30d92284a5e2356968eb7
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-03 09:26:35 +0100

    Added url for sending a direct.

--- notebook/notebook/direct/urls.py
+++ notebook/notebook/direct/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 # local imports
-from .views import inbox, Directs
+from .views import inbox, Directs, SendDirect
 
 urlpatterns = [
     path('', inbox, name='inbox'),
     path('directs/<username>', Directs, name='directs'),
+    path('send/', SendDirect, name='send_direct'),
 ]
