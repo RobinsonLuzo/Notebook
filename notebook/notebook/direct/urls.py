742ea672d5ca8028fb6a70475eafc076c3ea4e2a
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-06 10:22:14 +0100

    Added url for search operation. Also updated reference to search button in directs html page.

--- notebook/notebook/direct/urls.py
+++ notebook/notebook/direct/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # local imports
-from .views import inbox, Directs, SendDirect
+from .views import inbox, Directs, SendDirect, SearchUser
 
 urlpatterns = [
     path('', inbox, name='inbox'),
     path('directs/<username>', Directs, name='directs'),
     path('send/', SendDirect, name='send_direct'),
+    path('new/', SearchUser, name='usersearch'),
 ]
