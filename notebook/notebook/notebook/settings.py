b0165d5ca46b91a96daf9527f76535a6cd691984
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-07 18:16:16 +0100

    Added notifications app to registered apps in notebook. Also corrected spelling error in notification_type for notification model - choice now choices. Migrated change to db.

--- notebook/notebook/notebook/settings.py
+++ notebook/notebook/notebook/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'post',
     'comment',
     'direct',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- notebook/notebook/notifications/models.py
+++ notebook/notebook/notifications/models.py
@@ -27,7 +27,7 @@ class Notification(models.Model):
     post = models.ForeignKey('post.Post', on_delete=models.CASCADE, related_name='noti_post', blank=True, null=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='noti_from_user')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='noti_to_user')
-    notification_type = models.IntegerField(choice=NOTIFICATION_TYPES)
+    notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
     text_preview = models.CharField(max_length=90, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
