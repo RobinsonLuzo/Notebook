48325ed388f9c01faeeeb7f47b9c6c45f71acb12
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-07 19:22:23 +0100

    View for loading notifications created.

--- notebook/notebook/notifications/views.py
+++ notebook/notebook/notifications/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+# local import:
+from .models import Notification
 
 # Create your views here.
+
+def show_notifications(request):
+    """Load and return notification stream for a given user."""
+    user = request.user
+    notifications = Notification.objects.filter(user=user).order_by('-date')
+
+    template = loader.get_template('notifications.html')
+
+    context = {
+        'notifications': notifications,
+    }
+
+    return HttpResponse(template.render(context, request))
