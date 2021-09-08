ac5f5d40163a869c579f1bf70ec05b68d43dec03
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-08 10:24:45 +0100

    Added logic to allow for creation or deletion of notification based on comment or removal of comment by user.

--- notebook/notebook/comment/models.py
+++ notebook/notebook/comment/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save, post_delete
 
+# relative imports:
+from notifications.models import Notification
 from post.models import Post
 
 # Create your models here.
@@ -19,3 +22,27 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def user_comment_post(sender, instance, *args, **kwargs):
+        """Generates a notification when user comments on a post."""
+        comment = instance
+        post = comment.post
+        text_preview = comment.body[:90] #take only first 90 chars as a preview.
+        sender = comment.user
+
+        notify = Notification(post=post, sender=sender, user=post.user, text_preview=text_preview, notification_type=2)
+        notify.save()
+
+    def user_del_comment_post(sender, instance, *args, **kwargs):
+        """Deletes a notification when user removes their comment."""
+        comment = instance
+        post = comment.post
+        sender = comment.user
+
+        notify = Notification.objects.filter(post=post, user=post.user, sender=sender, notification_type=2)
+        notify.delete()
+
+
+# Comment-based notifications:
+post_save.connect(Comment.user_comment_post, sender=Comment)
+post_delete.connect(Comment.user_del_comment_post, sender=Comment)
