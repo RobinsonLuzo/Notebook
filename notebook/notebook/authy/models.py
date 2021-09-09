826c97f2da6c3db476695af888708d25e6b85406
RobinsonLuzo <bendanielfagan@gmail.com>
2021-09-09 14:07:40 +0100

    Fixed sizing of profile photos issue - now sized correctly and rounded as intended.

--- notebook/notebook/authy/models.py
+++ notebook/notebook/authy/models.py
@@ -1,17 +1,27 @@
-from django.db import models
-from django.contrib.auth.models import User
-from post.models import Post
+import os
 
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
 from django.db.models.signals import post_save
+from PIL import Image
+
+# relative import:
+from post.models import Post
 
 # Create your models here.
 def user_directory_path(instance, filename):
-    """
+	"""
     Given file will be uploaded to MEDIA_ROOT, form of: user(id)/filename.
-
     Note: user in this instance can be a user id in numeric or character.
     """
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+	profile_pic_name = 'user_{0}/profile.jpg'.format(instance.user.id)
+	full_path = os.path.join(settings.MEDIA_ROOT, profile_pic_name)
+
+	if os.path.exists(full_path):
+		os.remove(full_path)
+
+	return profile_pic_name
 
 
 class Profile(models.Model):
@@ -24,6 +34,20 @@ class Profile(models.Model):
 	created = models.DateField(auto_now_add=True)
 	favorites = models.ManyToManyField(Post)
 	picture = models.ImageField(upload_to=user_directory_path, blank=True, null=True, verbose_name='Picture')
+
+	def save(self, *args, **kwargs):
+		"""Override of picture as uploaded by user. Resize given picture and resave."""
+		super().save(*args, **kwargs)
+
+		SIZE = 350, 350
+
+		# check if a picture was actually uploaded as it can be empty.
+		if self.picture:
+			pic = Image.open(self.picture.path)
+			# for LANCZOS example see: https://www.programcreek.com/python/example/89920/PIL.Image.LANCZOS
+			pic.thumbnail(SIZE, Image.LANCZOS)
+			pic.save(self.picture.path)
+
 
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
